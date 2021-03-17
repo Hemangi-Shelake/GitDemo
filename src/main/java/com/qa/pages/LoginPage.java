1156779e070bc87548788d8391676914184c4f39
Hemangi-Shelake <hemangishelake@gmail.com>
2021-03-17 22:26:37 +0530

    added search page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,5 +5,9 @@ public class LoginPage {
 	public void loginPageMethod() {
 		System.out.println("Login Page");
 	}
+	
+	public void loginSetMethod() {
+		System.out.println("Login Set Method");
+	}
 
 }
